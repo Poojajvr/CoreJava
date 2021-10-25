21679c0e36ac2666b3b4a5714ad7b88cf7c3d7d4
poojajvr <poojajvxworkz@gmail.com>
2021-10-25 10:17:04 +0530

    methods

---
+++ methods/Agriculture.java
@@ -0,0 +1,18 @@
+class Agriculture
+{
+static String mangotrees()
+{
+System.out.println("full of mangoes");
+boolean farmer=true;
+if(farmer==true)
+{
+System.out.println("framers having good knowledge in agriculture");
+return"framers having good knowledge in agriculture";
+}
+else
+{
+System.out.println("framers don't have good knowledge in agriculture");
+return"framers don't have good knowledge in agriculture";
+}
+}
+}
