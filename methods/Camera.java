bbe0afd96c8a801a087c19586c4937290113675b
poojajvr <poojajvxworkz@gmail.com>
2021-10-25 10:31:38 +0530

    methods

---
+++ methods/Camera.java
@@ -0,0 +1,18 @@
+class Camera
+{
+static String pictures()
+{
+System.out.println("good looking pictures");
+int photos=9;
+if(photos==9)
+{
+System.out.println("photos having clarity");
+return("photos having clarity");
+}
+else
+{
+System.out.println("photos not having clarity");
+return("photos not having clarity");
+}
+}
+}
